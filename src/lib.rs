71477bb912eb554e0740ead292fff676589a27b8
James Correia <jcorr851@gmail.com>
2022-05-19 00:35:29 -0400

    First commit

---
+++ src/lib.rs
@@ -0,0 +1,153 @@
+#![allow(dead_code)]
+use std::ops;
+
+#[derive(Debug, Clone)]
+pub struct Matrix<const ROWS: usize, const COLS: usize> {
+    matrix: [[f64; COLS]; ROWS],
+}
+
+//Matrix with one column
+type ColumnVector<const SIZE: usize> = Matrix<SIZE, 1>;
+
+impl<const ROWS: usize, const COLS: usize> Matrix<ROWS, COLS> {
+    pub fn zero() -> Self {
+        Self {
+            matrix: [[0.0; COLS]; ROWS],
+        }
+    }
+
+    pub fn ones() -> Self {
+        Self {
+            matrix: [[1.0; COLS]; ROWS],
+        }
+    }
+}
+
+//For methods requiring square matrices
+impl<const ROWS: usize> Matrix<ROWS, ROWS> {
+    pub fn identity() -> Self {
+        let mut ret = Self::zero();
+        for i in 0..ROWS {
+            ret.matrix[i][i] = 1.0;
+        }
+
+        ret
+    }
+}
+
+//Matrix addition with + operator
+impl<const ROWS: usize, const COLS: usize> ops::Add<Matrix<ROWS, COLS>> for Matrix<ROWS, COLS> {
+    type Output = Matrix<ROWS, COLS>;
+    fn add(self, rhs: Self::Output) -> Self::Output {
+        let mut ret = Self::zero();
+
+        for ((i, lhs_row), rhs_row) in self.matrix.iter().enumerate().zip(rhs.matrix.iter()) {
+            for ((j, lhs_val), rhs_val) in lhs_row.iter().enumerate().zip(rhs_row.iter()) {
+                ret.matrix[i][j] = lhs_val + rhs_val;
+            }
+        }
+
+        ret
+    }
+}
+
+//Vector Addition with += operator
+impl<const ROWS: usize, const COLS: usize> ops::AddAssign<Matrix<ROWS, COLS>>
+    for Matrix<ROWS, COLS>
+{
+    fn add_assign(&mut self, rhs: Self) {
+        //TODO
+        todo!();
+    }
+}
+
+//Vector-Scalar multiplication with * operator
+impl<const ROWS: usize, const COLS: usize> ops::Mul<f64> for Matrix<ROWS, COLS> {
+    type Output = Matrix<ROWS, COLS>;
+    fn mul(self, rhs: f64) -> Self::Output {
+        for row in self.matrix.iter() {
+            row.map(|x| x * rhs);
+        }
+
+        self
+    }
+}
+
+#[derive(Debug)]
+pub struct Coordinate3d {
+    pos: ColumnVector<3>,
+    edges: Vec<Coordinate3d>,
+}
+
+//Translate a coordinate by a vector
+impl Coordinate3d {
+    fn translate(&mut self, vector: ColumnVector<3>) {
+        self.pos += vector;
+    }
+}
+
+#[derive(Debug)]
+pub struct Mesh {
+    vertices: Vec<Coordinate3d>,
+    origin: Coordinate3d,
+}
+
+impl Mesh {
+    pub fn translate(&mut self, vector: ColumnVector<3>) {
+        for vertex in self.vertices.iter_mut() {
+            vertex.translate(vector.clone()); //TODO: avoid clone() by implementing + by reference.
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn constructor_ones() {
+        assert_eq!(
+            Matrix::<3, 4>::ones().matrix,
+            [
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0],
+                [1.0, 1.0, 1.0, 1.0]
+            ]
+        );
+    }
+
+    #[test]
+    fn constructor_zero() {
+        assert_eq!(
+            Matrix::<3, 4>::zero().matrix,
+            [
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0]
+            ]
+        );
+    }
+
+    #[test]
+    fn constructor_identity() {
+        assert_eq!(
+            Matrix::<4, 4>::identity().matrix,
+            [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0]
+            ]
+        );
+    }
+
+    #[test]
+    fn test_matrix_add() {
+        fn matrix_add() {
+            assert_eq!(
+                (Matrix::<2, 2>::ones() + Matrix::<2, 2>::ones()).matrix,
+                [[2.0, 2.0], [2.0, 2.0]]
+            );
+        }
+    }
+}
